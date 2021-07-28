e539777bcbc0af674a2b108b589bf488592f6444
Michael Kistler <michaelkis213@yahoo.com>
2021-07-28 13:36:55 -0400

    Initial ML learning setup - not a good learner

--- Assets/Scripts/ML Scripts/Paddle_Agent.cs
+++ Assets/Scripts/ML Scripts/Paddle_Agent.cs
@@ -9,30 +9,47 @@ using Unity.MLAgents.Actuators;
 
 public class Paddle_Agent : Agent
 {
-    Rigidbody2D rBody;
-    Rigidbody2D ballrBody;
+    Rigidbody2D ballRigidBody;
     public Transform ball;
     public Transform bricks;
     public float speed;
     int numOfBricks;
     public GameObject bricksPrefab;
+    public float rightScreenLimit;
+    public float leftScreenLimit;
 
     void Start()
     {
-        rBody = GetComponent<Rigidbody2D>();
-        ballrBody = ball.GetComponent<Rigidbody2D>();
+        ballRigidBody = ball.GetComponent<Rigidbody2D>();
         numOfBricks = bricks.childCount;
+    }
+
+    void Update()
+    {
+        // if stmnt to prevent paddle from passing screen edge on left
+        if (this.transform.localPosition.x < leftScreenLimit)
+        {
+            this.transform.localPosition = new Vector3(leftScreenLimit, this.transform.localPosition.y, this.transform.localPosition.z);
+        }
+
+        // if stmnt to prevent paddle from passing screen edge on right
+        if (this.transform.localPosition.x > rightScreenLimit)
+        {
+            this.transform.localPosition = new Vector3(rightScreenLimit, this.transform.localPosition.y, this.transform.localPosition.z);
+        }
+
     }
 
     public override void OnEpisodeBegin()
     {
         if (numOfBricks == 0)
         {
-            bricks = Instantiate(bricksPrefab, new Vector3(0, 0, 0), Quaternion.identity).transform;
+            bricks = Instantiate(bricksPrefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform.parent).transform;
+            numOfBricks = bricks.childCount;
         }
-        ballrBody.velocity = Vector2.zero;
-        ball.localPosition = this.transform.localPosition + new Vector3(0, 1.0f, 0);
-        ballrBody.AddForce(Vector2.up * speed);
+        ballRigidBody.velocity = Vector2.zero;
+        ball.localPosition = this.transform.localPosition + new Vector3(0, 0.1f, 0);
+        ballRigidBody.AddForce(Vector2.up * speed);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -42,43 +59,57 @@ public class Paddle_Agent : Agent
         sensor.AddObservation(this.transform.localPosition);
 
         // Ball velocity
-        sensor.AddObservation(ballrBody.velocity.x);
-        sensor.AddObservation(ballrBody.velocity.y);
+        sensor.AddObservation(ballRigidBody.velocity.x);
+        sensor.AddObservation(ballRigidBody.velocity.y);
 
         // Number of bricks left
         sensor.AddObservation(bricks.childCount);
+
+        if (numOfBricks > 0)
+        {
+            // Direction to closest brick
+            Transform closestBrick = bricks.GetChild(0);
+            float distanceToClosestBrick = Vector3.Distance(this.transform.localPosition, closestBrick.localPosition);
+            for (var i = 1; i < bricks.childCount; i++)
+            {
+                if (distanceToClosestBrick > Vector3.Distance(this.transform.localPosition, bricks.GetChild(i).localPosition))
+                {
+                    closestBrick = bricks.GetChild(i);
+                    distanceToClosestBrick = Vector3.Distance(this.transform.localPosition, closestBrick.localPosition);
+                }
+            }
+            Vector3 directionToClosestBrick = this.transform.localPosition - closestBrick.localPosition;
+            sensor.AddObservation(directionToClosestBrick);
+        }
+        else
+        {
+            sensor.AddObservation(Vector3.zero);
+        }
     }
 
-    public float forceMultiplier = 10;
+    public float paddleSpeed = 10;
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Actions, size = 1
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = actionBuffers.ContinuousActions[0];
-        rBody.AddForce(controlSignal * forceMultiplier);
-
-        // Rewards
-        float distanceToTarget = Vector3.Distance(this.transform.localPosition, ball.localPosition);
-
-        // Ball collided
-        if (distanceToTarget < 1.42f)
-        {
-            SetReward(1.0f);
-        }
+        this.transform.Translate(controlSignal * paddleSpeed * Time.deltaTime);
 
         if (bricks.childCount < numOfBricks)
         {
+            SetReward(0.5f * (numOfBricks - bricks.childCount));
             numOfBricks = bricks.childCount;
-            SetReward(5.0f);
             if (numOfBricks == 0)
             {
                 EndEpisode();
             }
         }
 
-        // Ball missed
-        else if (ball.localPosition.y < this.transform.localPosition.y)
+        if (ball.localPosition.y < this.transform.localPosition.y - 1 ||
+            (ball.localPosition.x > 10 || ball.localPosition.x < -10) ||
+            ball.localPosition.y > 4.5)
         {
+            SetReward(-1.0f);
             EndEpisode();
         }
     }
